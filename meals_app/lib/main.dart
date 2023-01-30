0a8a0679e1621a6c1d3324517d7be62923cac97a
MARTIN_WANG <martin_wang@ms.dlp.com.tw>
2023-01-30 13:49:13 +0800

    update

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -77,18 +77,17 @@ class _MyAppState extends State<MyApp> {
         canvasColor: Color.fromRGBO(255, 254, 229, 1),
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-              bodySmall: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              bodyMedium: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              titleMedium: TextStyle(
-                fontSize: 20,
-                fontFamily: 'RobotoCondensed',
-                fontWeight: FontWeight.bold,
-              )
+            bodySmall: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
             ),
+            bodyMedium: TextStyle(
+              color: Color.fromRGBO(20, 51, 51, 1),
+            ),
+            titleMedium: TextStyle(
+              fontSize: 20,
+              fontFamily: 'RobotoCondensed',
+              fontWeight: FontWeight.bold,
+            )),
       ),
       // home: CategoriesScreen(),
       initialRoute: '/', // default is '/'
